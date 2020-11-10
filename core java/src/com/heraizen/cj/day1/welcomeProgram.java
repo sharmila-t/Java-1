53046667f8a3cf428b34cb367101a6c0a5fcfc3d
ranjithkr <ranjith.kr@heraizen.com>
2020-11-10 15:23:08 +0530

    testing

---
+++ core java/src/com/heraizen/cj/day1/welcomeProgram.java
@@ -0,0 +1,10 @@
+package com.heraizen.cj.day1;
+
+public class welcomeProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Java");
+	}
+
+}
